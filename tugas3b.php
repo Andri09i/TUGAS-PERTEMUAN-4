400f62c7138afb457caadf1f2f4f211ccadbe1fd
Andri09i <148644048+Andri09i@users.noreply.github.com>
2023-11-06 08:46:20 +0700

    TUGAS_PERTEMUAN_4
    TUGAS_PERTEMUAN_4

---
+++ tugas3b.php
@@ -0,0 +1,14 @@
+<?php
+
+$jawabanIsset = "Isset adalah fungsi yang digunakan untuk memeriksa apakah suatu variabel telah di atur dan memiliki nilai. <br><br>";
+$jawabanEmpty = "Empty adalah fungsi yang digunakan untuk memeriksa apakah suatu variabel kosong.";
+
+if(Isset($jawabanIsset)){
+	echo "$jawabanIsset";
+
+if(Empty(jawabanEmpty))
+	echo "$jawabanEmpty";
+ 
+}
+
+?>
